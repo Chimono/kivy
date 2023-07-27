ed78d62aab37278544dc85eff07f4763036f7de3
Sven <sven.kossenjans@web.de>
2023-07-27 14:14:36 +0200

    force _check_keyboard_shown if focus exists

--- kivy/core/window/window_sdl2.py
+++ kivy/core/window/window_sdl2.py
@@ -916,8 +916,8 @@ class WindowSDL(WindowBase):
             print("[window_sdl2] _check_keyboard_shown: self._win.is_keyboard_shown() is None")
             if self._sdl_keyboard.target:
                 print("[window_sdl2] _check_keyboard_shown: self._sdl_keyboard.target exsists")
-                keyboard_active = self.request_keyboard()
-                print("keyboard active: " + str(keyboard_active))
+                Clock.schedule_interval(self._check_keyboard_shown, 1 / 5.)
+                print("keyboard active: " + str(self._sdl_keyboard))
                 return False
             else:
                 print("[window_sdl2] _check_keyboard_shown: release keyboard")
